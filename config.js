f79da2dda22a73a4a9f267dd360869c202439dd7
ben ji <sacyfim@gmail.com>
2019-07-30 17:05:09 +0200

    Premières commandes msg

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.TOKEN = "NjA1NzA3MDkzODk0MjM0MTQz.XUBYzQ._Vuso72imsBORiYzW8OoUOCAGiA";
+exports.PREFIX = "B?";
+var PREFIXE = "b!";
--- main.js
+++ main.js
@@ -1,14 +1,22 @@
+// node main.js : pour lancer le bot
+// git add .
+// git commmit -m ""
+// git push
+
 const Discord = require("discord.js");
+const { TOKEN, PREFIX } = require("./config");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Je suis prêt !!");
 });
 
 client.on("message", msg => {
-  if (msg.content === "fuck yann") {
-    msg.reply("Oui fuck yann vraiment");
-  }
+  if (msg.author.bot) return;
+  const args = msg.content.split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+  var PREFIXE = "b!";
+  if (cmd === `${PREFIXE}ping`) msg.channel.send("Pong!");
 });
 
-client.login("NjA1NzA3MDkzODk0MjM0MTQz.XUAjgg.syhkm4-vOcE8xE9oFenzf7rakBg");
+client.login(TOKEN);
